91816da566a45afc9b5d3f32f905ab5941b263fb
Juan López <juanandrelopezcordova8@gmail.com>
2025-09-27 00:22:41 -0500

    first commit

---
+++ src/test/java/org/example/proyectofinaldwi/ProyectoFinalDwiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.proyectofinaldwi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalDwiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
